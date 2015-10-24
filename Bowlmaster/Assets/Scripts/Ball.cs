e994cbc1ca6fb4d3ff437cf8068d488382b01ca8
Rhys <rhysdavies1@gmail.com>
2015-10-25 04:21:56 +1030

    intial commit, has lane, ball, pins and render texture for pins.

---
+++ Bowlmaster/Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	private Rigidbody rigidBody;
+	private AudioSource audioSource;
+
+	public Vector3 launchVelocity;
+
+	// Use this for initialization
+	void Start () {
+
+		rigidBody = GetComponent<Rigidbody> ();
+		audioSource = GetComponent<AudioSource> ();
+
+		Launch ();
+	}
+
+	public void Launch ()
+	{
+		rigidBody.velocity = launchVelocity;
+		audioSource.Play ();
+	}
+}
---
+++ Bowlmaster/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public Ball ball;
+	private Vector3 offsetFromBall;
+
+	void Start () {
+
+		offsetFromBall = transform.position - ball.transform.position;
+	
+	}
+	
+	void Update () {
+
+		if (transform.position.z <= 1600){
+
+			transform.position = ball.transform.position + offsetFromBall;
+
+		}
+	
+	}
+}
